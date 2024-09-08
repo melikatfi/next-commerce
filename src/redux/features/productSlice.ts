2b4f4348f29933977a646798b668f38ca3865304
melikatfi <87155734+melikatfi@users.noreply.github.com>
2024-09-08 14:22:51 +0330

    productSlice changed based on IProduct

--- src/redux/features/productSlice.ts
+++ src/redux/features/productSlice.ts
@@ -1,23 +1,23 @@
-import { createSlice, PayloadAction} from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IProduct } from "../types";
 
 const initialState: IProduct = {
-    _id: "",
-    imgSrc: "",
-    fileKey: "",
-    name: "",
-    price: "",
-    category: ""
-}
+  id: "",
+  title: "",
+  img: "",
+  price: 0,
+  quantity: 0,
+};
 
 export const productSlice = createSlice({
-    name: "productSlice",
-    initialState,
-    reducers: {
-        setProduct: (state, action: PayloadAction<IProduct>) => {
-            return action.payload
-        }
-    }
-})
+  name: "productSlice",
+  initialState,
+  reducers: {
+    setProduct: (state, action: PayloadAction<IProduct>) => {
+      return action.payload;
+    },
+  },
+});
 
 export const { setProduct } = productSlice.actions;
 export default productSlice.reducer;
