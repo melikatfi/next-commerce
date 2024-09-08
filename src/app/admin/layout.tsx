984f0564cd25b17cb06df5e9ba96da156a344f58
melikatfi <87155734+melikatfi@users.noreply.github.com>
2024-09-08 14:06:31 +0330

    admin/layout edited

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,17 +1,26 @@
 "use client";
+
 import Login from "@/components/admin-panel/Login";
 import { useAppSelector } from "@/redux/hooks";
 import { useSession } from "next-auth/react";
 import React from "react";
 
-const layout = () => {
-  const isLoading = useAppSelector((store) => store.loadingReducer);
+// Change the component name to start with an uppercase letter
+const Layout = () => {
+  const isLoading = useAppSelector((store) => store.loadingReducer); // Use this variable or remove it
   const { data: session } = useSession();
 
+  // Use isLoading if needed; otherwise, remove it
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // Render Login component if no session is found
   if (!session?.user) {
     return <Login />;
   }
+
   return <div>this is layout</div>;
 };
 
-export default layout;
+export default Layout; // Export the component with an uppercase letter
